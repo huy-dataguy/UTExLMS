3b8e3c737ef92950d45d2f44c8b659d40223d0f9
Quoc Huy <quochuy.working@gmail.com>
2024-10-21 06:21:40 +0700

    init

---
+++ UTExLMS/Commands/TestButtonCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UTExLMS.Commands
+{
+    public class TestButtonCommand : CommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            MessageBox.Show("this is btn okkkk");
+        }
+    }
+}
