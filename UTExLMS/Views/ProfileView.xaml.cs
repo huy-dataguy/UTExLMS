5700483ec6678f67832b44411dcdf90da3123664
Quoc Huy <quochuy.working@gmail.com>
2024-10-25 02:41:21 +0700

    #21 finish profile

---
+++ UTExLMS/Views/ProfileView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using UTExLMS.Models;
+using UTExLMS.ViewModels;
+using UTExLMS.Models.DBContext;
+
+
+namespace UTExLMS.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class ProfileView : Page
+    {
+        public ProfileView()
+        {
+            InitializeComponent();
+            UTExLMSContext context = new UTExLMSContext();
+            this.DataContext = new ProfileLecturerViewModel(context);
+
+
+        }
+
+      
+    }
+}
