5d5534705e50dfd11f70b3174102b4f5d39330da
DOCUTEE <110187691+DOCUTEE@users.noreply.github.com>
2024-10-24 20:18:11 +0700

    #15-add present image when app running

--- UTExLMS/ViewModels/MainViewModel.cs
+++ UTExLMS/ViewModels/MainViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using UTExLMS.Views;
 
 namespace UTExLMS.ViewModels
@@ -11,6 +13,41 @@ namespace UTExLMS.ViewModels
     public class MainViewModel : ViewModelBase
     {
         private object _body;
+        private string _logoUtex;
+        public string logoUtex
+        {
+            get
+            {
+                string path = Environment.CurrentDirectory;
+                string path1 = Directory.GetParent(path).Parent.Parent.FullName;
+                return path1 + "\\Assets\\ute_logo.png";
+            }
+        }
+
+        private string _ringIcon;
+        public string ringIcon
+        {
+            get
+            {
+                string path = Environment.CurrentDirectory;
+                string path1 = Directory.GetParent(path).Parent.Parent.FullName;
+                return path1 + "\\Assets\\BellGray.png";
+            }
+        }
+
+        private string _avatar;
+        public string avatar
+        {
+            get
+            {
+                string path = Environment.CurrentDirectory;
+                string path1 = Directory.GetParent(path).Parent.Parent.FullName;
+                return path1 + "\\Assets\\avatar.png";
+            }
+        }
+
+
+
         public object Body
         {
             get => _body;
