ec02a742a04391a31bb08c3cbdebffcdbadeb508
Quoc Huy <quochuy.working@gmail.com>
2024-10-24 16:49:07 +0700

    #15 show list course

---
+++ UTExLMS/ViewModels/CourseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTExLMS.Models;
+
+namespace UTExLMS.ViewModels
+{
+    public class CourseViewModel : ViewModelBase
+    {
+        private readonly Class _class;
+        public int IdClass => _class.IdClass;
+        public string? NameClass => _class.NameClass;
+
+        public double? Progress => _class.Progress;
+        public int? IdStudent => _class.IdStudent;
+        public int? IdSubject => _class.IdSubject;
+        public int? IdLecturer => _class.IdLecturer;
+
+        public CourseViewModel(Class _class) 
+        {
+            _class = _class;
+        }
+
+    }
+}
---
+++ UTExLMS/ViewModels/StudentClassViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UTExLMS.Models;
+
+namespace UTExLMS.ViewModels
+{
+    public class StudentClassViewModel
+    {
+        private readonly UTExLMSContext _context;
+
+        public ObservableCollection<VwStudentCourse> StudentCourses { get; set; }
+        public ICommand LoadCoursesCommand { get; }
+
+        public StudentClassViewModel() { }
+        public StudentClassViewModel (UTExLMSContext context)
+        {
+            _context = context;
+            StudentCourses = new ObservableCollection<VwStudentCourse>();
+            LoadCoursesCommand = new RelayCommand(LoadStudentCourses);
+        }
+        private void LoadStudentCourses()
+        {
+            var studentId = 1;
+            var courses = _context.VwStudentCourses
+                .Where(c => c.IdStudent == studentId)
+                .ToList();
+
+            StudentCourses.Clear();
+            foreach (var course in courses)
+            {
+                StudentCourses.Add(course);
+            }
+        }
+    }
+}
